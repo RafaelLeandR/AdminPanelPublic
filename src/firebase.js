132cd463fcb5099c5dbd5678ca20eaa885a5b02c
Rafael Rosado <144853245+RafaelLeandR@users.noreply.github.com>
2026-09-22 00:35:53 -0300

    Update firebase.js
    Delete ids

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANroWwXGfRdG1LtVFHGomFUyBxwWt93Jc",
+  apiKey: "",
   authDomain: "loja-admin-458b5.firebaseapp.com",
   projectId: "loja-admin-458b5",
   storageBucket: "loja-admin-458b5.firebasestorage.app",
-  messagingSenderId: "154894503061",
-  appId: "1:154894503061:web:42c87304555e26cd8d5f9c",
-  measurementId: "G-0JCV74YKHN"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
